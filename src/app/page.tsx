34b843061d024b5e28c106ead249273deacce84b
juliscapucin <jscapucin@gmail.com>
2023-07-04 20:54:50 +0200

    integrate tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>
+    <main className='juli'>
       <div>
         <h1 className="text-5xl font-bold bg-green-700">
           Get started by editing&nbsp;!!!
