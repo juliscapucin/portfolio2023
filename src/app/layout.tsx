8c1751b638b94b17998f778583f8b781d46e4fc9
juliscapucin <jscapucin@gmail.com>
2023-07-09 14:03:56 +0200

    add constants + index for components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,25 @@
-import Header from '@/components/header'
-import '@/styles/styles.css'
-import { Inter } from 'next/font/google'
+import { Header } from '@/components';
+import '@/styles/styles.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Juli Scapucin',
-  description: 'Front-End Developer based in Amsterdam',
-}
+ title: 'Juli Scapucin',
+ description: 'Front-End Developer based in Amsterdam',
+};
 
 export default function RootLayout({
-  children,
+ children,
 }: {
-  children: React.ReactNode
+ children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        <main>
-          {children}
-        </main>
-      </body>
-    </html>
-  )
+ return (
+  <html lang='en'>
+   <body className={inter.className}>
+    <Header />
+    <main>{children}</main>
+   </body>
+  </html>
+ );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,23 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from 'next/image';
+import { CustomFilter, SearchBar } from '@/components';
 
 export default function Home() {
-	return (
-		<div className='juli'>
+ return (
+  <section className='juli'>
+   <div className='relative h-80'>
+    <Image
+     src='/pool.avif'
+     alt='Next.js Logo'
+     className='object-contain'
+     fill
+     priority
+    />
+   </div>
 
-			<div className="relative h-80">
-				<Image
-					src='/pool.avif'
-					alt='Next.js Logo'
-					className="object-contain"
-					fill
-					priority
-				/>
-			</div>
-
-			<div>
-				<a
-					href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-					target='_blank'
-					rel='noopener noreferrer'>
-					<h2>
-						Juli
-					</h2>
-					<p>Design & Code</p>
-				</a>
-			</div>
-		</div>
-	)
+   <div className='search'>
+    <SearchBar />
+    <CustomFilter />
+   </div>
+  </section>
+ );
 }
