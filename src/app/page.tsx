1bbc3192fee33e5090b784343d4a646804ada1c3
juliscapucin <jscapucin@gmail.com>
2023-07-10 22:09:59 +0200

    add grid elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,17 @@ import { CustomFilter, SearchBar } from '@/components';
 export default function Home() {
  return (
   <main>
-   <section className='grid gap-[1px] [&>*]:bg-colorBlack'>
-    <div className='h-32'></div>
-    <div className='h-32'></div>
-    <div className='h-32'></div>
-    <div className='h-32'></div>
-    <div className='h-32'></div>
-    <div className='h-32'></div>
+   <section className='grid gap-[1px]'>
+    <div className='h-64 bg-colorBlack'></div>
+    <div className='h-32 bg-colorBlack'></div>
+    <div className='grid grid-cols-3 gap-[1px] bg-colorWhite [&>*]:bg-colorBlack [&>*]:h-32'>
+     <div></div>
+     <div></div>
+     <div></div>
+    </div>
+    <div className='h-64 bg-colorBlack'></div>
+    <div className='h-32 bg-colorBlack'></div>
+    <div className='h-32 bg-colorBlack'></div>
    </section>
   </main>
  );
