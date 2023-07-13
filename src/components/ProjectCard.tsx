70bddddf9fc6df1dd3583dcc64ea97372cf86878
juliscapucin <jscapucin@gmail.com>
2023-07-13 10:58:38 +0200

    remove shadcn components + add image

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,22 +1,8 @@
 'use client';
 
+import Image from 'next/image';
 import { GridDiv } from '@/components';
 import { handleShallowClick } from '@/utils';
-
-import {
- Tooltip,
- TooltipContent,
- TooltipProvider,
- TooltipTrigger,
-} from '@/components/ui/tooltip';
-
-import {
- Select,
- SelectContent,
- SelectItem,
- SelectTrigger,
- SelectValue,
-} from '@/components/ui/select';
 
 interface ProjectCardProps {
  title: string;
@@ -31,28 +17,8 @@ export default function ProjectCard({
 }: ProjectCardProps) {
  return (
   <GridDiv top={false} right={true} bottom={true} left={true}>
-   <Select>
-    <SelectTrigger className='w-[180px]'>
-     <SelectValue placeholder='Theme' />
-    </SelectTrigger>
-    <SelectContent>
-     <SelectItem value='light'>Light</SelectItem>
-     <SelectItem value='dark'>Dark</SelectItem>
-     <SelectItem value='system'>System</SelectItem>
-    </SelectContent>
-   </Select>
-
-   <TooltipProvider>
-    <Tooltip>
-     <TooltipTrigger>Hover</TooltipTrigger>
-     <TooltipContent>
-      <p>Add to library</p>
-     </TooltipContent>
-    </Tooltip>
-   </TooltipProvider>
-
    <button
-    className='h-full w-full flex justify-start items-center text-7xl'
+    className='h-full w-full flex justify-start items-center text-7xl hover:opacity-50 transition-opacity'
     onClick={(e) => handleShallowClick(e, `/projects/${slug}`)}
    >
     <span className='font-headline text-7xl font-thin ml-8'>{title}</span>
