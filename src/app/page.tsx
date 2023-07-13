f124b3f38cb9cfcf1e97781a8ae2c7cbd716432f
juliscapucin <jscapucin@gmail.com>
2023-07-13 17:20:25 +0200

    add view all button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
      <GridElement top={false} right={true} bottom={true} left={false} />
     </div>
     <div className='h-64'>
-     <SectionTitle />
+     <SectionTitle title={'Latest Work'} />
     </div>
     {projectLinks.map((project, index) => {
      return (
@@ -62,6 +62,36 @@ export default function Home() {
      );
     })}
    </section>
+   <section>
+    <div className='h-64'>
+     <SectionTitle title={'Playground'} />
+    </div>
+    {projectLinks.map((project, index) => {
+     return (
+      <div className='h-32' key={index}>
+       <ProjectCard title={project.label} slug={project.slug} />
+      </div>
+     );
+    })}
+   </section>
+   <div className='h-32 flex'>
+    <GridElement
+     top={false}
+     right={true}
+     bottom={true}
+     left={true}
+     divClass='basis-3/4'
+    />
+    <GridElement
+     top={false}
+     right={true}
+     bottom={true}
+     left={false}
+     buttonLabel='View all'
+     width='basis-1/4'
+     divClass='flex justify-center items-center'
+    />
+   </div>
   </main>
  );
 }
