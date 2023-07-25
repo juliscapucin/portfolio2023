8ae351e481c6f0a8112c8cd1cedb328378ea3063
juliscapucin <jscapucin@gmail.com>
2023-07-25 22:28:05 +0200

    add scroller to scrolltrigger

--- src/components/shallowPage/ShallowPage.tsx
+++ src/components/shallowPage/ShallowPage.tsx
@@ -38,7 +38,7 @@ export default function ShallowPage({
 
  return (
   <div
-   className='fixed top-0 left-0 right-0 bottom-0 overflow-auto z-10'
+   className='scroll-trigger fixed top-0 left-0 right-0 bottom-0 overflow-auto z-10'
    ref={overlay}
    onClick={onClick}
   >
