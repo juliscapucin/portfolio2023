6f3dc41bf89aac3b64eeb46dd553c04f97ed41ca
juliscapucin <jscapucin@gmail.com>
2023-09-19 13:44:35 +0200

    detail-page: add disciplines + improve layout

--- src/app/@shallowPage/(.)[...work]/[slug]/page.tsx
+++ src/app/@shallowPage/(.)[...work]/[slug]/page.tsx
@@ -49,7 +49,7 @@ export default function Page({ params }: { params: { slug: string } }) {
  return (
   <ShallowPage>
    {/* Project header */}
-   <section className='relative w-full'>
+   <section className='relative w-full mb-1'>
     <h1 className='text-displayLarge'>{project?.title ? project.title : ''}</h1>
     <div className='grid grid-cols-12 mb-16'>
      <p className='text-headlineSmall col-span-6'>{project.subtitle}</p>
@@ -66,52 +66,11 @@ export default function Page({ params }: { params: { slug: string } }) {
     </div>
    </section>
    {/* Split screen */}
-   <section className='bg-colorWhite dark:bg-colorBlack'>
-    <section className='grid grid-cols-12 w-full'>
-     <div
-      ref={left}
-      className='col-span-8 [&>article]:h-screen [&>article>h2]:text-3xl [&>article>h2]:mb-8'
-     >
-      <div className='grid grid-cols-12 mt-16 mb-16'>
-       <p className='col-span-6'>{project.content}</p>
-      </div>
-      <article>
-       <h2>Scroll-based Animations</h2>
-       <p>
-        Easily animate elements as they enter or exit the viewport, or trigger
-        animations at specific scroll positions. From simple fade-ins to complex
-        parallax effects, ScrollTrigger gives you full control over the timing
-        and behavior of your animations.
-       </p>
-      </article>
-      <article>
-       <h2>Pinning and Sticky Elements</h2>
-       <p>
-        Create elements that remain fixed on the screen while the user scrolls.
-        Perfect for creating immersive headers, navigation menus, or sidebars
-        that stay in view and provide interactive functionality.
-       </p>
-      </article>
-      <article>
-       <h2>Scroll-based Progress Indicators</h2>
-       <p>
-        Use ScrollTrigger to create progress indicators that visualize the users
-        progress on a page. Highlighting sections, updating navigation menus, or
-        triggering animations based on the scroll progress can all be achieved
-        with ease.
-       </p>
-      </article>
-      <article>
-       <h2>Responsive and Adaptive Animations</h2>
-       <p>
-        ScrollTrigger seamlessly adapts to different screen sizes and
-        orientations. You can create animations that respond to the users
-        scrolling behavior while providing a consistent and optimized experience
-        across devices.
-       </p>
-      </article>
-     </div>
-     <div ref={right} className='col-span-4 h-[500px]'>
+   <section className='grid grid-cols-12 gap-1 w-full'>
+    {/* Left */}
+    <div ref={left} className='col-span-7 relative grid gap-1'>
+     <p className='h-[500px] pr-32 flex items-center'>{project.content}</p>
+     <div className='h-[600px] w-full overflow-hidden relative'>
       <Image
        src={project.coverImage}
        alt='photo'
@@ -120,9 +79,7 @@ export default function Page({ params }: { params: { slug: string } }) {
        fill
       />
      </div>
-    </section>
-    <section className='flex gap-32 w-full'>
-     <div ref={left2} className='w-1/4'>
+     <div className='h-[600px] w-full overflow-hidden relative'>
       <Image
        src={project.coverImage}
        alt='photo'
@@ -130,81 +87,84 @@ export default function Page({ params }: { params: { slug: string } }) {
        sizes='(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw'
        fill
       />
-      <CustomImage
-       src='/pool2.avif'
-       alt='pool photo'
-       height='h-screen'
-       width='w-full'
-       divClass='absolute top-0 left-0'
-      />
-      <CustomImage
-       src='/bus.avif'
-       alt='pool photo'
-       height='h-screen'
-       width='w-full'
-       divClass='absolute top-0 left-0'
-      />
-      <CustomImage
-       src='/door.avif'
-       alt='pool photo'
-       height='h-screen'
-       width='w-full'
-       divClass='absolute top-0 left-0'
-      />
-      <CustomImage
-       src='/parking.avif'
-       alt='pool photo'
-       height='h-screen'
-       width='w-full'
-       divClass='absolute top-0 left-0'
+     </div>
+     <div className='h-[600px] w-full overflow-hidden relative'>
+      <Image
+       src={project.coverImage}
+       alt='photo'
+       className='h-32 w-full object-cover'
+       sizes='(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw'
+       fill
       />
      </div>
-     <div
-      ref={right2}
-      className='w-3/4 [&>article]:h-screen [&>article>h2]:text-3xl [&>article>h2]:mb-8'
-     >
-      <div className='h-screen flex justify-start items-center'>
-       <h1 className='text-7xl' ref={title}>
-        GSAP ScrollTrigger
-       </h1>
-      </div>
-      <article>
-       <h2>Scroll-based Animations</h2>
-       <p>
-        Easily animate elements as they enter or exit the viewport, or trigger
-        animations at specific scroll positions. From simple fade-ins to complex
-        parallax effects, ScrollTrigger gives you full control over the timing
-        and behavior of your animations.
-       </p>
-      </article>
-      <article>
-       <h2>Pinning and Sticky Elements</h2>
-       <p>
-        Create elements that remain fixed on the screen while the user scrolls.
-        Perfect for creating immersive headers, navigation menus, or sidebars
-        that stay in view and provide interactive functionality.
-       </p>
-      </article>
-      <article>
-       <h2>Scroll-based Progress Indicators</h2>
-       <p>
-        Use ScrollTrigger to create progress indicators that visualize the users
-        progress on a page. Highlighting sections, updating navigation menus, or
-        triggering animations based on the scroll progress can all be achieved
-        with ease.
-       </p>
-      </article>
-      <article>
-       <h2>Responsive and Adaptive Animations</h2>
-       <p>
-        ScrollTrigger seamlessly adapts to different screen sizes and
-        orientations. You can create animations that respond to the users
-        scrolling behavior while providing a consistent and optimized experience
-        across devices.
-       </p>
-      </article>
+    </div>
+
+    {/* Right */}
+    <div ref={right} className='col-span-5 h-[500px]'>
+     <Image
+      src={project.coverImage}
+      alt='photo'
+      className='w-full object-cover'
+      sizes='(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw'
+      fill
+     />
+    </div>
+   </section>
+
+   {/* Disciplines */}
+   <section className='w-full p-32 flex gap-8 justify-center items-center'>
+    {project.disciplines.map((discipline, index) => {
+     return (
+      <span className='text-displaySmall'>
+       {index !== 0 && ' | '}
+       {discipline}
+      </span>
+     );
+    })}
+   </section>
+
+   <section className='grid grid-cols-12 w-full relative gap-1'>
+    {/* Left 2 */}
+    <div ref={left2} className='col-span-5 h-[500px]'>
+     <Image
+      src={project.coverImage}
+      alt='photo'
+      className='w-full object-cover'
+      sizes='(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw'
+      fill
+     />
+    </div>
+
+    {/* Right 2 */}
+    <div ref={right2} className='col-span-7 relative grid gap-1'>
+     <div className='h-[600px] w-full overflow-hidden relative'>
+      <Image
+       src={project.coverImage}
+       alt='photo'
+       className='w-full object-cover'
+       sizes='(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw'
+       fill
+      />
      </div>
-    </section>
+     <div className='h-[600px] w-full overflow-hidden relative'>
+      <Image
+       src={project.coverImage}
+       alt='photo'
+       className='h-32 w-full object-cover'
+       sizes='(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw'
+       fill
+      />
+     </div>
+     <div className='h-[600px] w-full overflow-hidden relative'>
+      <Image
+       src={project.coverImage}
+       alt='photo'
+       className='h-32 w-full object-cover'
+       sizes='(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw'
+       fill
+      />
+     </div>
+    </div>
    </section>
   </ShallowPage>
  );
