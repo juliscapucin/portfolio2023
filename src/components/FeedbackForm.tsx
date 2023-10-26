1fb1be7d935fc3f9d42c5b68c073297e00744d38
juliscapucin <jscapucin@gmail.com>
2023-10-26 14:36:37 +0200

    feedback: add interactive labels

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,30 +1,70 @@
 'use client';
 
-type FormInputProps = {
- label: string;
- type: string;
- id: string;
-};
+import { useState } from 'react';
 
-const FormInput = ({ label, type, id }: FormInputProps) => {
- return (
-  <div className='flex flex-col'>
-   <label htmlFor='name'>{label}:</label>
-   {type === 'textarea' ? (
-    <textarea id='name' placeholder={label} rows={5} />
-   ) : (
-    <input type='text' id='name' placeholder={label} />
-   )}
-  </div>
- );
+type FormData = {
+ name: string;
+ message: string;
 };
 
 export default function FeedbackForm() {
+ const [formData, setFormData] = useState<FormData>({ name: '', message: '' });
+
+ const handleChange = (
+  e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+ ) => {
+  const { name, value } = e.target;
+  setFormData((prevState) => ({ ...prevState, [name]: value }));
+ };
+
+ const handleSubmit = (e: React.FormEvent) => {
+  e.preventDefault();
+  console.log('Form Data:', formData);
+  // Handle form submission, e.g., send data to API
+ };
+
  return (
   <section className='grid grid-cols-12'>
-   <form action='' className='col-span-4'>
-    <FormInput label='Name' type='text' id='name' />
-    <FormInput label='Message' type='textarea' id='message' />
+   <form onSubmit={handleSubmit} className='col-span-4'>
+    <div className='flex flex-col border-b border-secondary mt-16 relative'>
+     <label
+      className={`absolute top-0 left-0 transition-transform duration-200 ease-in-out ${
+       formData.name ? '-translate-y-10' : 'opacity-0'
+      }`}
+      htmlFor={'name'}
+     >
+      Name:
+     </label>
+     <input
+      className='appearance-none bg-transparent border-none w-full'
+      type='text'
+      id={'name'}
+      name='name'
+      placeholder={'Name'}
+      value={formData.name}
+      onChange={handleChange}
+     />
+    </div>
+
+    <div className='flex flex-col border-b border-secondary mt-16 relative'>
+     <label
+      className={`absolute top-0 left-0 transition-transform duration-200 ease-in-out ${
+       formData.message ? '-translate-y-10' : 'opacity-0'
+      }`}
+      htmlFor={'message'}
+     >
+      Message:
+     </label>
+     <textarea
+      className='appearance-none bg-transparent border-none w-full'
+      id={'message'}
+      name='message'
+      placeholder={'Message'}
+      rows={5}
+      value={formData.message}
+      onChange={handleChange}
+     />
+    </div>
     <button className='mt-16'>Submit</button>
    </form>
   </section>
