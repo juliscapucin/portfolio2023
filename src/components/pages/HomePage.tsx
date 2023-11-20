9eb409c3e7e1442875936163446410e9a5fa111d
juliscapucin <jscapucin@gmail.com>
2023-11-20 17:57:55 +0100

    deployment-fixes: improve HeroMobile

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -41,7 +41,7 @@ export default function HomePage({ allProjects }: { allProjects: Project[] }) {
 
  return (
   <div ref={pageRef} className='page home-page main-page overflow-hidden'>
-   <GridDiv divClass='overflow-hidden min-h-screen' bottom={true}>
+   <GridDiv divClass='overflow-hidden h-screen min-h-screen' bottom={true}>
     {breakpoint === 'mobile' && <HeroMobile />}
     {breakpoint === 'desktop' && <HeroDesktop />}
    </GridDiv>
