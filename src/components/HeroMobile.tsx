3b6dc0db24fe94f5d5e821a958417382384efb1a
juliscapucin <jscapucin@gmail.com>
2023-11-20 18:09:50 +0100

    deployment-fixes: improve home mobile layotu

--- src/components/HeroMobile.tsx
+++ src/components/HeroMobile.tsx
@@ -17,7 +17,7 @@ const HeroMobile = () => {
  }, [nameRef]);
 
  return (
-  <div className='hero__mobile min-h-5/6 h-screen flex flex-col justify-between'>
+  <div className='hero__mobile h-5/6 flex flex-col justify-between'>
    <div className='grid grid-cols-6'>
     {/* Status */}
     <GridDiv divClass='col-span-4 sm:col-span-3 md:col-span-1'>
@@ -25,6 +25,18 @@ const HeroMobile = () => {
     </GridDiv>
    </div>
    <div>
+    {/* Marquee */}
+    <GridDiv divClass='h-64 flex items-center' top={true}>
+     <Marquee speed={150}>
+      <h2 className='text-displayMedium whitespace-nowrap px-2'>
+       Design & Web Development
+      </h2>
+      <h2 className='text-displayMedium whitespace-nowrap px-2'>
+       Design & Web Development
+      </h2>
+     </Marquee>
+    </GridDiv>
+
     {/* Name */}
     <GridDiv divClass='overflow-hidden h-32 flex items-center' top={true}>
      <h1 ref={nameRef} className='text-[11vw] whitespace-nowrap'>
@@ -32,23 +44,12 @@ const HeroMobile = () => {
      </h1>
     </GridDiv>
 
-    {/* Marquee */}
-    <GridDiv divClass='h-32 flex items-center' top={true}>
-     <Marquee speed={150}>
-      <h2 className='text-[11vw] whitespace-nowrap px-2'>
-       Design & Web Development
-      </h2>
-      <h2 className='text-[11vw] whitespace-nowrap px-2'>
-       Design & Web Development
-      </h2>
-      <h2 className='text-[11vw] whitespace-nowrap px-2'>
-       Design & Web Development
-      </h2>
-     </Marquee>
-    </GridDiv>
-
     {/* Number */}
-    <GridDiv divClass='h-fit overflow-hidden flex items-center' top={true}>
+    <GridDiv
+     divClass='h-fit overflow-hidden flex items-center'
+     top={true}
+     bottom={true}
+    >
      <h3
       ref={numberRef}
       className='number font-normal tracking-tightest leading-none'
@@ -56,11 +57,6 @@ const HeroMobile = () => {
       23
      </h3>
     </GridDiv>
-
-    {/* Theme */}
-    {/* <GridDiv divClass='col-span-full row-span-1 flex justify-end' top={true}>
-    <ThemeSwitcher variant='body' />
-   </GridDiv> */}
    </div>
   </div>
  );
