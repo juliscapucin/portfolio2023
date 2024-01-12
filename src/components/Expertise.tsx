c03eb1aedd48e7ddffaff07dd96d01c4eed06c42
juliscapucin <jscapucin@gmail.com>
2024-01-12 17:44:56 +0100

    projects-filter-improvements: fix sectionTitle responsiveness

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -31,15 +31,15 @@ export default function Expertise() {
  return (
   <section className='w-full col-span-12 md:grid grid-cols-12'>
    <div className='col-span-12 overflow-clip'>
-    <SectionTitle title='Expertise' />
+    <SectionTitle title='What I do' />
    </div>
    {expertises &&
     expertises.map((expertise) => (
      <Fragment key={expertise.title}>
-      <h3 className='col-start-6 col-span-7 text-displaySmall mt-64'>
+      <h3 className='col-start-6 col-span-7 text-headlineLarge sm:text-displaySmall mt-64'>
        {expertise.title}
       </h3>
-      <ul className='col-start-6 col-span-7 mt-8'>
+      <ul className='col-start-6 col-span-7 mt-16 sm:mt-8'>
        {expertise.items.map((item) => (
         <ExpertiseItem key={item} expertise={item} />
        ))}
