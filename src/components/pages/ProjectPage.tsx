a81daa27e6851942878e0e1518d0af92e87a9616
juliscapucin <jscapucin@gmail.com>
2024-01-13 17:48:43 +0100

    loader-on-project-page: add suspense

--- src/components/pages/ProjectPage.tsx
+++ src/components/pages/ProjectPage.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef, useLayoutEffect } from 'react';
+import { useRef, useLayoutEffect, Suspense } from 'react';
 
 import { CldImage } from 'next-cloudinary';
 import gsap from 'gsap';
@@ -64,41 +64,46 @@ export default function ProjectPage({
   <ShallowPage isShallow={isShallow} key={project.slug}>
    {project ? (
     <>
-     <ProjectsMenuThumbs allProjects={allProjects} />
+     {/* Title */}
+     <h1 className='mb-4 pt-32 z-30 text-displaySmall md:text-displayMedium lg:text-displayLarge'>
+      {project.title}
+     </h1>
 
-     <section className='relative w-full pt-32 z-30 bg-primary'>
-      {/* Title */}
-      <h1 className='text-displaySmall md:text-displayMedium lg:text-displayLarge mb-4'>
-       {project.title}
-      </h1>
+     {/* Page */}
+     <Suspense fallback={<Loader />}>
+      <ProjectsMenuThumbs allProjects={allProjects} />
 
-      {/* Header */}
-      <div ref={headerRef} className='md:grid grid-cols-12'>
-       {/* Cover Image */}
-       <div className={`col-span-7 block overflow-clip aspect-square relative`}>
-        <CldImage
-         src={`portfolio2023/work/${project.slug}/01`}
-         alt={project.coverImage.alt}
-         sizes='(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw'
-         fill
-         priority
-        />
-       </div>
-       <div className='md:col-span-4 md:pl-16 mt-8'>
-        {/* Description */}
-        <div className='mb-16'>
-         <p className='text-titleLarge md:text-headlineSmall mt-16 md:mt-0'>
-          {project.description}
-         </p>
+      <section className='relative w-full z-30 bg-primary'>
+       {/* Header */}
+       <div ref={headerRef} className='md:grid grid-cols-12'>
+        {/* Cover Image */}
+        <div
+         className={`col-span-7 block overflow-clip aspect-square relative`}
+        >
+         <CldImage
+          src={`portfolio2023/work/${project.slug}/01`}
+          alt={project.coverImage.alt}
+          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw'
+          fill
+          priority
+         />
         </div>
-        {/* Project Info */}
-        <ProjectInfo info={project.info} />
+        <div className='md:col-span-4 md:pl-16 mt-8'>
+         {/* Description */}
+         <div className='mb-16'>
+          <p className='text-titleLarge md:text-headlineSmall mt-16 md:mt-0'>
+           {project.description}
+          </p>
+         </div>
+         {/* Project Info */}
+         <ProjectInfo info={project.info} />
+        </div>
        </div>
-      </div>
-     </section>
+      </section>
 
-     {/* Split Screen */}
-     {project.images && <ProjectSplitScreen project={project} />}
+      {/* Split Screen */}
+      {project.images && <ProjectSplitScreen project={project} />}
+     </Suspense>
     </>
    ) : (
     <div className='min-w-screen min-h-screen bg-primary flex items-center justify-center'>
