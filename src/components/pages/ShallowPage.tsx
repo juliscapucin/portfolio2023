adb18a4669eff73dc83c388eea6e72bc95788e4b
juliscapucin <jscapucin@gmail.com>
2024-05-13 11:38:22 +0200

    improvements: update next.js + fix max-w on page transition

--- src/components/pages/ShallowPage.tsx
+++ src/components/pages/ShallowPage.tsx
@@ -82,7 +82,7 @@ export default function ShallowPage({ children, isShallow }: Props) {
     ref={isShallow ? shallowPageRef : pageRef}
     className={`${
      isShallow && 'shallow-page'
-    } page scroll-trigger fixed top-0 left-0 bottom-0 right-0 mx-auto pl-8 pr-16 lg:pr-8 bg-primary max-w-desktop overflow-y-scroll overflow-x-clip z-10`}
+    } page scroll-trigger fixed top-0 left-0 bottom-0 right-0 mx-auto pl-8 pr-16 lg:pr-8 max-w-desktop overflow-y-scroll overflow-x-clip z-10`}
    >
     <div
      className='wrapper max-w-desktop overflow-clip m-auto mt-0 py-32 bg-primary'
